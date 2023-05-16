090a1d7ccf29d45a5b8d9b197d7c2234a382aa53
jungyeeun00 <74102394+jungyeeun00@users.noreply.github.com>
2023-05-17 00:00:42 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 23.56 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/README.md
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 제일 작은 수 제거하기 - 12935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935?language=java) 
 
 ### 성능 요약
 
-메모리: 81.6 MB, 시간: 15.02 ms
+메모리: 71.9 MB, 시간: 23.56 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,10 +1,7 @@
 import java.util.Arrays;
 class Solution {
     public int[] solution(int[] arr) {
-        if(arr[0]==10){
-            int [] res = {-1};
-            return res;
-        }
+        if(arr[0]==10) return new int[]{-1};
         return Arrays.stream(arr).filter(t->t>Arrays.stream(arr).min().getAsInt()).toArray();
     }
 }
