e1fdc8e9c8851bc28b31037ec02e9a8ceda23154
jungyeeun00 <74102394+jungyeeun00@users.noreply.github.com>
2023-05-18 00:46:11 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.08 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/README.md
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/README.md
@@ -1,10 +1,10 @@
 # [level 1] 수박수박수박수박수박수? - 12922 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12922) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12922?language=java) 
 
 ### 성능 요약
 
-메모리: 68.7 MB, 시간: 0.07 ms
+메모리: 74.6 MB, 시간: 0.08 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,12 +1,13 @@
 class Solution {
     public String solution(int n) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<n;i++){
-            if(i%2==0)
-                sb.append("수");
-            else
-                sb.append("박");
-        }
-        return sb.toString();
+        // StringBuilder sb = new StringBuilder();
+        // for(int i=0;i<n;i++){
+        //     if(i%2==0)
+        //         sb.append("수");
+        //     else
+        //         sb.append("박");
+        // }
+        // return sb.toString();
+        return new String(new char[n/2+1]).replace("\0", "수박").substring(0, n);
     }
 }
