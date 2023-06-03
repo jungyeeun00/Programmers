14c90178bca52fc7e17a0293487bfda4d5085c65
jungyeeun00 <74102394+jungyeeun00@users.noreply.github.com>
2023-06-03 14:30:04 +0900

    [level 1] Title: 최소직사각형, Time: 1.96 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/README.md
+++ 프로그래머스/lv1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=java) 
 
 ### 성능 요약
 
-메모리: 85.2 MB, 시간: 1.17 ms
+메모리: 85.5 MB, 시간: 1.96 ms
 
 ### 구분
 
--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -1,17 +1,22 @@
 class Solution {
     public int solution(int[][] sizes) {
-        for (int i = 0; i < sizes.length; i++) {
-            if (sizes[i][1]>sizes[i][0]){
-                int tmp = sizes[i][1];
-                sizes[i][1]=sizes[i][0];
-                sizes[i][0]=tmp;
-            }
-        }
-        int width = sizes[0][0];
-        int height = sizes[0][1];
-        for (int i = 0; i < sizes.length; i++) {
-            if(sizes[i][0]>width) width = sizes[i][0];
-            if(sizes[i][1]>height) height = sizes[i][1];
+        // for (int i = 0; i < sizes.length; i++) {
+        //     if (sizes[i][1]>sizes[i][0]){
+        //         int tmp = sizes[i][1];
+        //         sizes[i][1]=sizes[i][0];
+        //         sizes[i][0]=tmp;
+        //     }
+        // }
+        // int width = sizes[0][0];
+        // int height = sizes[0][1];
+        // for (int i = 0; i < sizes.length; i++) {
+        //     if(sizes[i][0]>width) width = sizes[i][0];
+        //     if(sizes[i][1]>height) height = sizes[i][1];
+        // }
+        int width = 0, height = 0;
+        for(int[] card:sizes){
+            width = Math.max(width, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
         }
         return width*height;
     }
