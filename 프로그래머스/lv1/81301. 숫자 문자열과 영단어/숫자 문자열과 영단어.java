ac27d4dd531f33cb18ddfd0a7443b79c5c8d543a
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2023-06-08 16:17:28 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,14 +1,32 @@
 class Solution {
     public int solution(String s) {
-        return Integer.parseInt(s.replace("zero", "0")
-                .replace("one", "1")
-                .replace("two", "2")
-                .replace("three", "3")
-                .replace("four", "4")
-                .replace("five", "5")
-                .replace("six", "6")
-                .replace("seven", "7")
-                .replace("eight", "8")
-                .replace("nine", "9"));
+        // return Integer.parseInt(s.replace("zero", "0")
+        //         .replace("one", "1")
+        //         .replace("two", "2")
+        //         .replace("three", "3")
+        //         .replace("four", "4")
+        //         .replace("five", "5")
+        //         .replace("six", "6")
+        //         .replace("seven", "7")
+        //         .replace("eight", "8")
+        //         .replace("nine", "9"));
+        
+        String[] num = {
+                "zero",
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine"
+        };
+        
+        for(int i=0;i<num.length;i++){
+            s = s.replace(num[i], Integer.toString(i));
+        }
+        return Integer.parseInt(s);
     }
 }
