cb286793897cd72b524d09aa4dd028370ee029b4
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2023-06-11 00:02:59 +0900

    [unrated] Title: 명예의 전당 (1), Time: 1.82 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            list.add(score[i]);
+            if (list.size()>k){
+                list.remove(Integer.valueOf(list.stream().mapToInt(t -> t).min().getAsInt()));
+            }
+            answer[i] = list.stream().mapToInt(t -> t).min().getAsInt();
+        }
+        return answer;
+    }
+}
