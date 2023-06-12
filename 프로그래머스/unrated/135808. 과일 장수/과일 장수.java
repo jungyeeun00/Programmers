0bdf66f4c6168b640b48460ab73377454874163c
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2023-06-12 23:22:04 +0900

    [unrated] Title: 과일 장수, Time: 2.15 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        int []sortedScore = Arrays.stream(score).sorted().toArray();
+        int idx = 0;
+        for (int i = sortedScore.length-1; i >= 0; i--) {
+            idx++;
+            if (idx%m==0) answer += sortedScore[i] * m;
+        }
+        return answer;
+    }
+}
