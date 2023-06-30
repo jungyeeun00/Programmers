fa91ac68b60d0f85ca56c90e1c262db36173151d
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2023-06-30 14:51:54 +0900

    [unrated] Title: 덧칠하기, Time: 0.92 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int idx = 0;
+        for(int i=1;i<=n;i++){
+            if(section[idx]==i){
+                i = i + m - 1;
+                if(section[idx]<=i){
+                    while(true){
+                        if (idx==section.length-1||section[idx]>i) break;
+                        idx++;
+                    }
+                }
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
