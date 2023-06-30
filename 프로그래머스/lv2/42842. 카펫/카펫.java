6eec9555ba4abcaa1827be553d349ce572424d2e
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2023-06-30 10:45:17 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,14 +1,19 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {0, 0};
-        for(int i=3;i<brown+yellow/2;i++){
-            if((brown+yellow)%i==0){
-                answer[1]=i;
-                answer[0] = brown / 2 + 2 - answer[1];
-                if(answer[1]*answer[0]==brown+yellow)
-                    return answer;
-            }
-        }
-        return answer;
+        // int[] answer = {0, 0};
+        // for(int i=3;i<brown+yellow/2;i++){
+        //     if((brown+yellow)%i==0){
+        //         answer[1]=i;
+        //         answer[0] = brown / 2 + 2 - answer[1];
+        //         if(answer[1]*answer[0]==brown+yellow)
+        //             return answer;
+        //     }
+        // }
+        // return answer;
+        
+        int a = brown/2+2;
+        int b = brown+yellow;
+        
+        return new int[]{(int)(a+Math.sqrt(a*a-4*b))/2, (int)(a-Math.sqrt(a*a-4*b))/2};
     }
 }
