446508abe8caa3854ff38dbc442ae98c2622ec9b
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2023-07-09 01:50:50 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,14 +1,12 @@
 class Solution {
     public long solution(int n) {
         long []answer = new long[n+1];
-        if(n==1) return 1;
-        else if(n==2) return 2;
         answer[0]=1;
         answer[1]=1;
         for(int i=2;i<=n;i++){
             answer[i]=(answer[i-1]+answer[i-2])%1234567;
         }
-        
+
         return answer[n];
     }
 }
