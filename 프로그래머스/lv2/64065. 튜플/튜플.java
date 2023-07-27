11f2260a9670857eba82d93b91bdae9fa48fcfae
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2023-07-28 00:26:08 +0900

    [level 2] Title: 튜플, Time: 0.58 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -3,14 +3,12 @@ class Solution {
     public int[] solution(String s) {
         String[] arr = s.replace("{", "").replace("}}", "").split("},");
         Arrays.sort(arr, (o1, o2) -> o1.length()-o2.length());
-        Set<Integer> set = new HashSet<>();
+        Set<String> set = new HashSet<>();
         int[] answer = new int[arr.length];
         int idx = 0;
         for (String s1 : arr) {
-            String[] tuple = s1.split(",");
-            for (String s2 : tuple) {
-                int a1 = Integer.parseInt(s2);
-                if (set.add(a1)) answer[idx++] = a1;
+            for (String s2 : s1.split(",")) {
+                if (set.add(s2)) answer[idx++] = Integer.parseInt(s2);
             }
         }
         return answer;
