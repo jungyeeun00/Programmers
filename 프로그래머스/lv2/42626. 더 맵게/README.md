6b19d863ce47f7a8adaf19160637e688f5a50f3d
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2023-08-21 00:05:24 +0900

    [level 2] Title: 더 맵게, Time: 1380.52 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/README.md
+++ 프로그래머스/lv2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 178 MB, 시간: 1806.42 ms
+메모리: 122 MB, 시간: 1380.52 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -9,21 +9,31 @@ class Solution {
             pq.add(i);
         }
 
-        while (true) {
-            if (pq.stream().noneMatch(t->t<K))
-                break;
-            if (pq.isEmpty()) return -1;
+//         while (true) {
+//             if (pq.stream().noneMatch(t->t<K))
+//                 break;
+//             if (pq.isEmpty()) return -1;
+//             int first = pq.poll();
+//             if (pq.isEmpty()) return -1;
+//             int second = pq.poll();
+//             int mix = first + (second * 2);
+//             answer++;
+//             pq.add(mix);
+
+
+//         }
+        
+//         if (pq.peek()<K) return -1;
+        
+        while (pq.size() > 1 && pq.peek() < K) {
             int first = pq.poll();
-            if (pq.isEmpty()) return -1;
             int second = pq.poll();
             int mix = first + (second * 2);
             answer++;
             pq.add(mix);
-
-
         }
-        
-        if (pq.peek()<K) return -1;
+
+        if (pq.size() == 1 && pq.peek() < K) return -1;
         return answer;
     }
 }
