65606302cbb82ff14c1c38c9b90615f8be8b18c3
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2023-08-20 23:56:10 +0900

    [level 2] Title: 더 맵게, Time: 1806.42 ms, Memory: 178 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i : scoville) {
+            pq.add(i);
+        }
+
+        while (true) {
+            if (pq.stream().noneMatch(t->t<K))
+                break;
+            if (pq.isEmpty()) return -1;
+            int first = pq.poll();
+            if (pq.isEmpty()) return -1;
+            int second = pq.poll();
+            int mix = first + (second * 2);
+            answer++;
+            pq.add(mix);
+
+
+        }
+        
+        if (pq.peek()<K) return -1;
+        return answer;
+    }
+}
