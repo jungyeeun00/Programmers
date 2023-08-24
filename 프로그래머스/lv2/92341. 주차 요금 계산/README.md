783e9cbf4a67a344c092ad3701d7b32b30205a35
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2023-08-24 23:40:59 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.37 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/README.md
+++ 프로그래머스/lv2/92341. 주차 요금 계산/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주차 요금 계산 - 92341 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341?language=java) 
 
 ### 성능 요약
 
-메모리: 79.1 MB, 시간: 0.47 ms
+메모리: 73.7 MB, 시간: 0.37 ms
 
 ### 구분
 
--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,69 +1,29 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        HashMap<String, Stack> map = new HashMap<>();
-        HashMap<String, Integer> car = new HashMap<>();
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
 
         for (String record : records) {
-            String [] arr = record.split(" ");
-            if (arr[2].equals("IN")) {
-                if (map.containsKey(arr[1])) {
-                    Stack<Integer> stack = map.get(arr[1]);
-                    String[] time = arr[0].split(":");
-                    int minutes = Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
-                    stack.push(minutes);
-                    map.replace(arr[1], stack);
-                } else {
-                    Stack<Integer> stack = new Stack<>();
-                    String[] time = arr[0].split(":");
-                    int minutes = Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
-                    stack.push(minutes);
-                    map.put(arr[1], stack);
-                }
-            } else {
-                Stack<Integer> stack = map.get(arr[1]);
-                int prev = stack.pop();
-                String[] time = arr[0].split(":");
-                int next = Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
-                if (car.containsKey(arr[1])) {
-                    car.replace(arr[1], car.get(arr[1]) + (next - prev));
-                } else {
-                    car.put(arr[1], next - prev);
-                }
-            }
+            String[] arr = record.split(" ");
+            int add = arr[2].equals("IN") ? -1 : 1;
+            String[] times = arr[0].split(":");
+            int time = Integer.parseInt(times[0]) * 60 + Integer.parseInt(times[1]);
+            treeMap.put(arr[1], treeMap.getOrDefault(arr[1], 0) + time * add);
         }
 
-        for (String s : map.keySet()) {
-            if (!map.get(s).isEmpty()) {
-                int prev = (int) map.get(s).pop();
-                int next = 23 * 60 + 59;
-                if (car.containsKey(s)) {
-                    car.replace(s, car.get(s) + (next - prev));
-                } else {
-                    car.put(s, next - prev);
-                }
-            }
-        }
-
-        String[] cars = new String[car.size()];
-
+        int[] answer = new int[treeMap.size()];
         int idx = 0;
-        for (String s : car.keySet()) {
-            cars[idx++] = s;
-            int time = car.get(s);
+        for (int time : treeMap.values()) {
+            if (time <= 0) {
+                time += 60 * 23 + 59;
+            }
             int fee;
             if (time <= fees[0]) {
                 fee = fees[1];
             } else {
                 fee = (int) (fees[1] + Math.ceil((double) (time - fees[0]) / fees[2]) * fees[3]);
             }
-            car.replace(s, fee);
-        }
-        Arrays.sort(cars);
-        int[] answer = new int[car.size()];
-
-        for (int i = 0; i < cars.length; i++) {
-            answer[i] = car.get(cars[i]);
+            answer[idx++] = fee;
         }
         return answer;
     }
