2633d1e9bbf3fc51e7f451c4b9a5df2c45184841
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2024-01-17 23:18:16 +0900

    [Silver II] Title: 연속합, Time: 780 ms, Memory: 109840 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -6,19 +6,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-
-        long dp[] = new long[N+1];
-        int arr[] = new int[N];
+        int[] arr = new int[N];
 
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
         }
 
-        dp[1] = arr[0];
-        long max = dp[1];
-
-        for (int i=1;i<=N;i++) {
-            dp[i] = Math.max(arr[i-1], arr[i-1]+dp[i-1]);
+        int[] dp = new int[N + 1];
+        int max = dp[1] = arr[0];
+        for (int i = 2; i <= N; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i - 1], arr[i - 1]);
             max = Math.max(dp[i], max);
         }
 
