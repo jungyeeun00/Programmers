7297f87b44c275ea72e027ca9b36d4cf64309127
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2024-01-20 23:43:57 +0900

    [Silver I] Title: RGB거리, Time: 272 ms, Memory: 20212 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -11,20 +11,23 @@ public class Main {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < 3; j++) {
                 dp[i][j] = sc.nextInt();
-                if (i==0) continue;
+            }
+
+            if (i==0) continue;
+
+            for (int j = 0; j < 3; j++) {
                 if (j == 0) {
                     dp[i][j] += Math.min(dp[i - 1][1], dp[i - 1][2]);
                 } else if (j == 1) {
                     dp[i][j] += Math.min(dp[i - 1][0], dp[i - 1][2]);
                 } else {
-                    dp[i][j] += Math.min(dp[i - 1][0], dp[i - 1][1]);
+                    dp[i][j] += Math.min(dp[i - 1][1], dp[i - 1][0]);
                 }
             }
         }
 
-        int min = Math.min(Math.min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]);
 
-        System.out.println(min);
+        System.out.println(Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2])));
 
     }
 }
