50432939a58ac79e3b412af96aea6daa793e3f73
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2024-03-15 23:54:08 +0900

    [Gold V] Title: LCS, Time: 164 ms, Memory: 19104 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -7,22 +7,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String str1 = st.nextToken();
+        String a = st.nextToken();
         st = new StringTokenizer(br.readLine());
-        String str2 = st.nextToken();
-        int[][] lcs = new int[str1.length()+1][str2.length()+1];
-        int answer = 0;
+        String b = st.nextToken();
 
-        for (int i = 1; i <= str1.length(); i++) {
-            for (int j = 1; j <= str2.length(); j++) {
-                lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+        int[][] lcs = new int[a.length() + 1][b.length() + 1];
+
+        for (int i = 1; i <= a.length(); i++) {
+            for (int j = 1; j <= b.length(); j++) {
+                lcs[i][j] = Math.max(lcs[i][j - 1], lcs[i - 1][j]);
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
                     lcs[i][j] = lcs[i - 1][j - 1] + 1;
                 }
-                answer = Math.max(answer, lcs[i][j]);
             }
         }
 
-        System.out.println(answer);
+        System.out.println(lcs[a.length()][b.length()]);
     }
 }
