4fc1bd202b4da1dedd8b52768369b396dd576120
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2024-04-25 00:12:43 +0900

    [Silver III] Title: 칸토어 집합, Time: 336 ms, Memory: 22732 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -2,41 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 4779. 칸토어 집합 
-public class Main{
-	static int n;
-	static StringBuilder answer;
+public class Main {
+    static StringBuilder sb;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        while ((str = br.readLine()) != null) {
+            n = Integer.parseInt(str);
+            sb = new StringBuilder();
+            int len = (int) Math.pow(3, n);
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+            for (int i = 0; i < len; i++) {
+                sb.append("-");
+            }
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str;
-		while ((str = br.readLine()) != null) {
-			n = Integer.parseInt(str);
-			answer =new StringBuilder();
-			int len = (int) Math.pow(3, n);
-            //길이가 3^n 이고 '-'로 이루어진 문자열 미리 만들어 놓고, 재귀함수 시작하기 
-			for (int i = 0; i < len; i++) {
-				answer.append("-");
-			}
-			
-			func(0, len); // 시작 인덱스, 시작 길이
-			System.out.println(answer);
+            cantorian(0, len);
+            System.out.println(sb);
+        }
+    }
 
-		}
-	}
+    static void cantorian(int start, int size) {
+        if (size == 1) {
+            return;
+        }
 
-	public static void func(int start,int size) { //시작 인덱스, 문자열 사이즈
-		if(size==1) { //문자열 사이즈가 1이되면 리턴
-			return;
-		}
-		int newSize=size/3;
-        // 3등분한 문자열의 2번째 부분 공백처리
-		for(int i=start+newSize; i<start+2*newSize; i++) {
-			answer.setCharAt(i, ' ');
-		}
-		
-		func(start, newSize); // 3등분한 문자열에서 첫번째 부분
-		func(start+2*newSize, newSize); // 3등분한 문자열의 세번째 부분 
-	}
+        int newSize = size / 3;
+        for (int i = start + newSize; i < start + newSize * 2; i++) {
+            sb.setCharAt(i, ' ');
+        }
+        cantorian(start, newSize);
+        cantorian(start + newSize * 2, newSize);
+    }
+
+
 }
