379c960889d2438216844152f5cf9130c908cbd0
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2024-04-29 23:11:01 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 16636 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb;
+    static int N, M;
+    static int[]arr;
+//    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        sb = new StringBuilder();
+
+//        visit = new boolean[N];
+        arr = new int[M];
+
+        backTracking(0);
+
+        System.out.println(sb);
+    }
+
+    static void backTracking(int depth) {
+        if (depth == M) {
+            for (int i :
+                    arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (depth > 0 && i + 1 < arr[depth - 1]) {
+                continue;
+            }
+            arr[depth] = i + 1;
+            backTracking(depth + 1);
+        }
+    }
+
+}
