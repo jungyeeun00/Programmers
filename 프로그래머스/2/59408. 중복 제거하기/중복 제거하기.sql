4b8a473998def5cfbfcf6081b4221da1e756302c
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2024-05-24 08:34:46 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,4 +1,9 @@
 -- 코드를 입력하세요
-SELECT count(DISTINCT NAME) as 'COUNT'
-from ANIMAL_INS
+# SELECT count(DISTINCT NAME) as 'COUNT'
+# from ANIMAL_INS
+# where NAME is not null
+
+
+select count(distinct name) count
+from ANIMAL_INS 
 where NAME is not null
