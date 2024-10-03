44b3817e609851938d7e21573f44064711369d67
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2024-10-03 11:17:29 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 60.48 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0;i<arr.length;i++){
+            if(!stack.isEmpty()&&stack.peek()==arr[i]){
+                stack.pop();
+            }
+            stack.push(arr[i]);
+        }
+        
+        answer = new int[stack.size()];
+        for(int i=answer.length-1;i>=0;i--){
+            answer[i]=stack.pop();
+        }
+
+        return answer;
+    }
+}
