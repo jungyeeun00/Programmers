fcc2ca9b71de671de0548ef021d8eaf595e7cd7e
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2024-10-08 12:34:28 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,32 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static long dp[] = new long[101];
+    public static void main(String []args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(br.readLine());
+//        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = sc.nextInt();
+        long dp[] = new long[101];
+        dp[1]=dp[2]=dp[3]=1;
+        dp[4]=dp[5]=2;
+        for (int i = 6; i <= 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
 
-        dp[1] = 1;
-        dp[2] = 1;
-        dp[3] = 1;
-        dp[4] = 2;
-        dp[5] = 2;
-        dp[6] = 3;
-        dp[7] = 4;
-        dp[8] = 5;
-        dp[9] = 7;
-        dp[10] = 9;
-
-        while (N-- > 0) {
-            int num = sc.nextInt();
-
-            for (int i = 11; i <= num; i++) {
-                dp[i] = dp[i - 1] + dp[i - 5];
-            }
-            System.out.println(dp[num]);
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            System.out.println(dp[N]);
         }
 
     }
+
 }
