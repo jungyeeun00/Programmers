cd800c2c41cfa9ef46668cd506453c52126d2476
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2024-10-11 23:44:17 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 18260 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -1,22 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static long dp[] = new long[1000001];
+    public static void main(String []args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(br.readLine());
+//        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        dp[1] = 1;
-        dp[2] = 2;
-
-        int n = sc.nextInt();
-
-        for (int i=3;i<=n;i++){
+        int[] dp = new int[N];
+        dp[0] = 1;
+        if (N>=2) dp[1] = 2;
+        for (int i = 2; i < N; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2])%15746;
         }
+        System.out.println(dp[N-1]);
 
-        System.out.println(dp[n]);
+
+
     }
-
 
 }
