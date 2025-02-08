d54a9266c5aff13c0dcc393d214aaf5c0a7c7d96
yeeun chung <74102394+jungyeeun00@users.noreply.github.com>
2025-02-08 22:30:27 +0900

    [Silver III] Title: 터렛, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.java
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -1,28 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-	public static void main(String []args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		for(int i=0;i<t;i++)
-			System.out.println(PositionCount(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt()));
-		
-		sc.close();
-	}
-	public static int PositionCount(int x1,int y1, int r1, int x2, int y2, int r2) {
-		double d=Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));	
-			
-		if(d==0) {
-			if(r1==r2) return -1;
-			else return 0;
-		}
-		
-		if(Math.abs(r1-r2)>d) return 0;
-		
-		if(Math.abs(r1+r2)==d||Math.abs(r1-r2)==d)
-			return 1;
-		else if(Math.abs(r1+r2)<d) 
-			return 0;		
-		else return 2;
-	}
+    static StringBuilder sb = new StringBuilder();
+//    static StringTokenizer st;
+
+    public static void main(String []args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            sb.append(checkPos(br.readLine())).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static int checkPos(String str) {
+        int[] arr = new int[6];
+        StringTokenizer st = new StringTokenizer(str);
+        for (int i = 0; i < 6; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (arr[0] == arr[3] && arr[1] == arr[4] && arr[2] == arr[5]) { //두 사람의 위치와 거리가 동일할 때
+//            sb.append("-1\n");
+            return -1;
+        }
+
+        int d = (int) (Math.pow(arr[0] - arr[3], 2) + Math.pow(arr[1] - arr[4], 2));
+        if (d > Math.pow(arr[2] + arr[5], 2)) {     //원이 닿아있지 않을 때
+//            sb.append("0\n");
+            return 0;
+        } else if (d < Math.pow(arr[2] - arr[5], 2)) {  //원 안에 원이 있을 때
+//            sb.append("0\n");
+            return 0;
+        } else if (d == Math.pow(arr[2] - arr[5], 2)) { //두 원이 내접할 때
+//            sb.append("1\n");
+            return 1;
+        } else if (d == Math.pow(arr[2] + arr[5], 2)) { //두 원이 외접할 때
+//            sb.append("1\n");
+            return 1;
+        } else {
+//        } else if (d < Math.pow(arr[2] + arr[5], 2)) { //원이 겹칠 때 (두 원의 중점 사이 거리 < 두 원의 반지름의 합
+//            sb.append("2\n");
+            return 2;
+        }
+
+    }
+
 }
